110caaba0641c1780ec3758d98f1aeee6f25a451
Agata Filho <agata78@gmail.com>
2016-09-29 16:22:02 -0400

    Continue work on `/orders/import` view function

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/orders/import', methods=['POST'])
 def load_orders():
-    data = request.data.split('\n')
+    data = request.data.strip().split('\n')
     print 'DATA: ', data
     columns = data[0].split('|')
     print 'COLUMNS', columns
@@ -23,37 +23,80 @@ def load_orders():
         order_id = mapped['id']
         o_name = mapped['name']
         o_email = mapped['email']
+        o_state = mapped['state']
         o_zip_code = mapped['zipcode']
-        valid = is_valid(line)
-        errors=[]
+        # if order is valid populate valid column with `1` otherwise `0`.
+        v = is_valid(line)
+        if v:
+            valid = 1
+        else:
+            valid = 0
 
-        current_entry = Order(order_id=order_id,
+        current_line = Order(order_id=order_id,
                                 o_name=o_name,
                                 o_email=o_email,
+                                o_state=o_state,
                                 o_zip_code=o_zip_code,
                                 valid=valid,
                                 errors=errors)
-        db.session.add(current_entry)
+        db.session.add(current_line)
         db.session.commit()
-    return 'Data loaded. To check for potential errors go to: ' #TODO
+
+    return 'Data loaded. To check for errors query orders.db errors table.' 
             #query ^^^^^^^ the db for errors and its user
 
 
 def load_errors():
-    if errors: #TODO is_valid will return collection of errors if any
-        for error in errors:
-            e_name = error
+    pass
 
 # Validation functions
 
 def is_valid(line):
     # return 1 if order is valid, 0 otherwise
+    rules = valid_state(line) and valid_zipcode(line), valid_age(line), valid_email(line), \
+            valid_zip_sum(line), valid_domain(line), valid_automatically(line)
+    return rules
+
+def valid_state(line): # instatce of Order class
+    print("made it in valid state!")
+    """No wine can ship to New Jersey, Connecticut, Pennsylvania, Massachusetts,
+    Illinois, Idaho or Oregon."""
+    
+    state = order.o_state 
+    invalid_state = state in ('OR', 'NJ')
+    if invalid_state:
+        rule = 'Allowed states'
+        new_row = Error(e_name=rule, order_key=order.primary_key)
+        db.session.add(new_row)
+        db.session.commit()
+        return False
+    return True   
+
+
+def valid_zipcode(line):
+    """Valid zip codes must be 5 or 9 digits."""
     return True
 
-def valid_state():
+def valid_age(line):
+    """Everyone ordering must be 21 or older."""
     return True
 
-def valid_zipcode():
+def valid_email(line):
+    """ Email address must be valid."""
+    return True
+
+def valid_zip_sum(line):
+    """The sum of digits in a zip code may not exceed 20
+       ("90210": 9+0+2+1+0 = 12)."""
+    return True
+
+def valid_domain(line):
+    """Customers from NY may not have .net email addresses."""
+    return True
+
+def valid_automatically(line):
+    """If the state and zip code of the following record is 
+    the same as the current record, it automatically passes."""
     return True
 
 
@@ -69,3 +112,5 @@ if __name__ == "__main__":
 
     init_db(app)
     app.run()
+
+    
