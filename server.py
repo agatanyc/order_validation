c6f5b6dc249abedf124e728e08c67a104004da90
Agata Filho <agata78@gmail.com>
2016-09-30 21:35:27 -0400

    Continue work on `orders\import` view function.

--- server.py
+++ server.py
@@ -13,53 +13,57 @@ def index():
 @app.route('/orders/import', methods=['POST'])
 def load_orders():
     data = request.data.strip().split('\n')
-    columns = data[0].strip().split('|')
-    current_line = None
-    for line in data[1:]:
-        split_line = line.strip().split('|')
-        mapped = {}
-        for i in range(len(columns)):
-            mapped[columns[i]] = split_line[i]
-        # populate `Order` table
-        order_id = mapped['id']
-        o_name = mapped['name']
-        o_email = mapped['email']
-        o_state = mapped['state']
-        o_zip_code = mapped['zipcode']
-        o_DOB = mapped['birthday']
-
-        next_line = Order(order_id=order_id,
-                                o_name=o_name,
-                                o_email=o_email,
-                                o_state=o_state,
-                                o_zip_code=o_zip_code,
-                                o_DOB = o_DOB,
-                                valid=0,
-                                errors=[])
-        # if order is valid populate valid column with `1` otherwise `0`.
-        set_valid(current_line, next_line)
-        if current_line:
-            db.session.add(current_line)
-            db.session.commit()
-        current_line = next_line
-    set_valid(next_line, None)
-    db.session.add(next_line)
-    db.session.commit()
-
+    if len(data) > 1:
+        columns = data[0].strip().split('|')
+        current_line = None
+        for line in data[1:]:
+            split_line = line.strip().split('|')
+            # mapped example:
+            # mapped = {'name': 'Stone Dominguez', 'zipcode': '05938',
+            #           'id': '4877', 'state': 'IA', 'birthday': 'Feb 27, 1963',
+            #          'email': 'ligula.Aliquam.erat@semperegestasurna.com'}
+            mapped = {}
+            for i in range(len(columns)):
+                mapped[columns[i]] = split_line[i]
+            # populate `Order` table
+            order_id = mapped['id']
+            o_name = mapped['name']
+            o_email = mapped['email']
+            o_state = mapped['state']
+            o_zip_code = mapped['zipcode']
+            o_DOB = mapped['birthday']
+            next_line = Order(order_id=order_id,
+                                    o_name=o_name,
+                                    o_email=o_email,
+                                    o_state=o_state,
+                                    o_zip_code=o_zip_code,
+                                    o_DOB = o_DOB,
+                                    valid=0,
+                                    errors=[])
+            # if order is valid populate valid column with `1` otherwise `0`.
+            # set_valid(current_line, next_line)
+            if current_line:
+                set_valid(current_line, next_line)
+                db.session.add(current_line)
+                db.session.commit()
+            current_line = next_line
+        set_valid(current_line, None)
+        db.session.add(current_line)
+        db.session.commit()
+# TODO return JSON
     return 'Data loaded. To check for errors query orders.db errors table.' 
-            #query ^^^^^^^ the db for errors and its user
 
-
-def load_errors():
-    pass
 
 # Validation functions
 
 def set_valid(line, next_line):
-    """return False if any of the validation functions return False,
-       otherwise True."""
-    if not line or not next_line:
-        continue
+    """Return False if any of the validation functions return False,
+        otherwise True.
+        If the state and zip code of the following record is 
+        the same as the current record, it automatically passes."""
+    if next_line and line.o_zip_code == next_line.o_zip_code and \
+         line.o_state == next_line.o_state:
+         line.valid = 1
     else:
         state_valid = valid_state(line)
         zipcode_valid = valid_zipcode(line)
@@ -67,12 +71,12 @@ def set_valid(line, next_line):
         email_valid = valid_email(line)
         zip_sum_valid = valid_zip_sum(line)
         domain_valid = valid_domain(line)
-        automatically_valid = valid_automatically(line)
         if state_valid and zipcode_valid and age_valid and email_valid and \
-           zip_sum_valid and domain_valid and automatically_valid:
+           zip_sum_valid and domain_valid:
             line.valid = 1
         else:
             line.valid = 0
+    return line.valid
 
 def valid_state(line): # instatce of Order class
     """No wine can ship to New Jersey, Connecticut, Pennsylvania, Massachusetts,
@@ -83,8 +87,6 @@ def valid_state(line): # instatce of Order class
         rule = 'Allowed states'
         new_row = Error(e_name=rule, order_key=line.primary_key)
         line.errors.append(new_row)
-        #db.session.add(new_row)
-        #db.session.commit()
         return False
     return True   
 
@@ -92,9 +94,13 @@ def valid_state(line): # instatce of Order class
 def valid_zipcode(line):
     """Valid zip codes must be 5 or 9 digits."""
     zipcode = line.o_zip_code
-    if len(zipcode) in [5, 9] and zipcode.isdigit():
-        return True
-    return False
+    invalid_zip = len(zipcode) not in [5, 9] and zipcode.isdigit()
+    if invalid_zip:
+        rule = 'Zipcode length'
+        new_row = Error(e_name=rule, order_key=line.primary_key)
+        line.errors.append(new_row)
+        return False
+    return True
 
 def _is_21(dob):
     # date of birth format example: "Sep 30, 1995"
@@ -109,38 +115,47 @@ def _is_21(dob):
 def valid_age(line):
     """Everyone ordering must be 21 or older."""
     dob = line.o_DOB
-    return _is_21(dob)
+    if not _is_21(dob):
+        rule = 'Allowed age'
+        new_row = Error(e_name=rule, order_key=line.primary_key)
+        line.errors.append(new_row)
+        return False
+    return True
 
 def valid_email(line):
     """ Email address must be valid."""
     email = line.o_email
     is_valid = validate_email(email)
-    return is_valid
+    if not is_valid:
+        rule = 'Email validation'
+        new_row = Error(e_name=rule, order_key=line.primary_key)
+        line.errors.append(new_row)
+        return False
+    return True
 
 def valid_zip_sum(line):
     """The sum of digits in a zip code may not exceed 20
        ("90210": 9+0+2+1+0 = 12)."""
     zipcode = line.o_zip_code
-    return sum(int(x) for x in zipcode) <= 20
+    if not sum(int(x) for x in zipcode) <= 20:
+        rule = 'Zipcode sum'
+        new_row = Error(e_name=rule, order_key=line.primary_key)
+        line.errors.append(new_row)
+        return False
+    return True
 
 def valid_domain(line):
     """Customers from NY may not have .net email addresses."""
     email = line.o_email
     ln = len(email)
-    return not email[ln - 4:] == '.net'
-
-def valid_automatically(line):
-    """If the state and zip code of the following record is 
-    the same as the current record, it automatically passes."""
+    invalid_domain = (email[ln - 4:] == '.net')
+    invalid_state = (line.o_state == 'NY')
+    if invalid_domain and invalid_state:
+        rule = '.net domain'
+        new_row = Error(e_name=rule, order_key=line.primary_key)
+        line.errors.append(new_row)
+        return False
     return True
-
-
-
-# mapped example:
-# mapped = {'name': 'Stone Dominguez', 'zipcode': '05938',
-#           'id': '4877', 'state': 'IA', 'birthday': 'Feb 27, 1963',
-#          'email': 'ligula.Aliquam.erat@semperegestasurna.com'}
-
 
 if __name__ == "__main__":
     app.debug = True
