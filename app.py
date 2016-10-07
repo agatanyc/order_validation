1d98270e4cdaf1ac0869fa834ec1a753e6af7781
Agata Filho <agata78@gmail.com>
2016-10-06 22:24:02 -0400

    Update aplication's config.

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ from flask import Flask
 # create the aplication
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///orders.db'
+app.config.update(
+    DEBUG=True,
+    ALLOWED_STATES=['NJ', 'PA', 'MA', 'IL', 'CT', 'ID', 'OR'],
+    ZIPCODE_SUM=True,
+    ZIPCODE_LENGTH= True,
+    ALLOWED_AGE=True,
+    EMAIL_VALIDATION=True,
+    NET_DOMAIN=True
+)
+
